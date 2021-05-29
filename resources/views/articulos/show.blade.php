ddb95654c38e098d25f7a3ce37b380571095be3c
Abdellah <abdellahbukadduh99@gmail.com>
2021-05-29 14:03:16 +0200

    show.blade.php (ver articulo) mejorado

--- resources/views/articulos/show.blade.php
+++ resources/views/articulos/show.blade.php
@@ -1,25 +1,110 @@
 @extends ('layouts.app')
 
+
 @section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-8">
 
-<h1 class="text-center mb-4">{{ $articulo->titulo }}</h1>
+            <article>
 
-<span class="font-weight-bold">Autor:
-    {{ $articulo->autor->name }}
-</span>
+                <header class="mb-4">
 
+                    <h1 class="fw-bolder mb-1">{{ $articulo->titulo }}</h1>
 
-<article class="my-3 col align-self-center">
+                    <div class="text-muted fst-italic mb-2">Publicado {{ $articulo->created_at}} por <a href="#">{{ $articulo->autor->name }}</a></div>
 
-{!! $articulo->contenido !!}
+                    <a class="badge bg-secondary text-decoration-none link-light" href="#!">Web Design</a>
+                    <a class="badge bg-secondary text-decoration-none link-light" href="#!">Freebies</a>
+                </header>
 
-</article>
+                <figure class="mb-4"><img class="img-fluid rounded" src="{{ route('ruta.index') }}/storage/{{ $articulo->imagen }}" alt="Miniatura"></figure>
+                <!-- Post content-->
+                <section class="mb-5">
+                    {!! $articulo->contenido !!}
+                </section>
+            </article>
+            <!-- Comments section-->
+            <section class="mb-5">
+                <div class="card bg-light">
+                    <div class="card-body">
+                        <!-- Comment form-->
+                        <form class="mb-4"><textarea class="form-control" rows="3" placeholder="Join the discussion and leave a comment!"></textarea></form>
+                        <!-- Comment with nested comments-->
+                        <div class="d-flex mb-4">
+                            <!-- Parent comment-->
+                            <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..."></div>
+                            <div class="ms-3">
+                                <div class="fw-bold">Commenter Name</div>
+                                If you're going to lead a space frontier, it has to be government; it'll never be private enterprise. Because the space frontier is dangerous, and it's expensive, and it has unquantified risks.
+                                <!-- Child comment 1-->
+                                <div class="d-flex mt-4">
+                                    <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..."></div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold">Commenter Name</div>
+                                        And under those conditions, you cannot establish a capital-market evaluation of that enterprise. You can't get investors.
+                                    </div>
+                                </div>
+                                <!-- Child comment 2-->
+                                <div class="d-flex mt-4">
+                                    <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..."></div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold">Commenter Name</div>
+                                        When you put money directly to a problem, it makes a good headline.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Single comment-->
+                        <div class="d-flex">
+                            <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..."></div>
+                            <div class="ms-3">
+                                <div class="fw-bold">Commenter Name</div>
+                                When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- Side widgets-->
+        <div class="col-lg-4">
 
-
-@endsection
-
-<!--
-<div>
-<img src="{{ route('ruta.index') }}/storage/{{ $articulo->imagen }}" alt="Miniatura">
+            <!-- Categories widget-->
+            <div class="card mb-4">
+                <div class="card-header">Categories</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="#!">Web Design</a></li>
+                                <li><a href="#!">HTML</a></li>
+                                <li><a href="#!">Freebies</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6">
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="#!">JavaScript</a></li>
+                                <li><a href="#!">CSS</a></li>
+                                <li><a href="#!">Tutorials</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Side widget-->
+            <div class="card mb-4">
+                <div class="card-header">Side Widget</div>
+                <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!
+                    You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!
+                    You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!
+                    You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!
+                    You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!
+                    You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!
+                    You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--->
+@endsection
