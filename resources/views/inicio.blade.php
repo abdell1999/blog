a9303ced9c65b7f2bf9e7308bd6ea9aa96065f51
Abdellah <abdellahbukadduh99@gmail.com>
2021-05-29 13:12:39 +0200

    Mejorada pagina principal

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 <h1 class="text-center mt-3">Articulos más Recientes</h1>
+<br>
+        <!--
         <hr>
         <div class="row">
             @foreach ($articulos as $articulo)
@@ -12,6 +14,41 @@
                 </div>
             @endforeach
         </div>
+    -->
+
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                @foreach ($articulos as $articulo)
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="{{ route('ruta.index') }}/storage/{{ $articulo->imagen }}" alt="Miniatura">
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">{{ $articulo->titulo }}</h5>
+                                <!-- Product price-->
+                                <!-- $40.00 - $80.00-->
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{ route('articulos.show', ['articulo'=> $articulo->id]) }}">Ver artículo</a></div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+
+
+
+
+
+            </div>
+        </div>
+    </section>
 
 
 
