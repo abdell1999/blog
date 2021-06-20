4a02652c1d753848fa9690aa38255ba345864d6a
Abdellah <abdellahbukadduh99@gmail.com>
2021-06-20 17:30:39 +0200

    agregado enlace al perfil en la barra

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">
+                                    <a class="dropdown-item" href="{{ route('users.show', ['user'=> Auth::user()->id]) }}">
                                     Perfil
                                     </a>
 
