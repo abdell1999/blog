c1ea101f717acd6ee61fc90abeab69ac32c18128
Abdellah <abdellahbukadduh99@gmail.com>
2021-06-20 13:25:29 +0200

    Cambiada paginacion a 8 por pagina

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -9,7 +9,7 @@ class InicioController extends Controller
 
 
     public function index(){
-        $articulos = Articulo::orderBy('created_at', 'DESC')->paginate(12);
+        $articulos = Articulo::orderBy('created_at', 'DESC')->paginate(8);
         return view('inicio')->with('articulos', $articulos);
     }
 
